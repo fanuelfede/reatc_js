cdd4140d8bd96962f423652bc0d9898ab1557600
fanuelfede <fedefanuel@gmail.com>
2022-01-31 20:33:26 -0300

    menu
    Menu

---
+++ desafio_1_ecommerce/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function NavBar (){
+
+    return <nav> 
+        <ul class="pipoca">    
+        <li><a class="botonete" href="#">Novedades</a></li>
+        <li><a class="botonete" href="#">Productos</a></li>
+        <li><a class="botonete" href="#">Servicios</a></li>
+        <li><a class="botonete" href="#">Encontranos</a></li>
+        </ul> 
+    </nav>
+
+}
+
+export default NavBar;
+
