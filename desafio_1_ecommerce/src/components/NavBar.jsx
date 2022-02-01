7d861da0f0730bd433a937b7239c3a5e4107de51
fanuelfede <fedefanuel@gmail.com>
2022-01-31 21:30:39 -0300

    class/className
    corregir class

--- desafio_1_ecommerce/src/components/NavBar.jsx
+++ desafio_1_ecommerce/src/components/NavBar.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 function NavBar (){
 
     return <nav> 
-        <ul class="pipoca">    
-        <li><a class="botonete" href="#">Novedades</a></li>
-        <li><a class="botonete" href="#">Productos</a></li>
-        <li><a class="botonete" href="#">Servicios</a></li>
-        <li><a class="botonete" href="#">Encontranos</a></li>
+        <ul classNameName="pipoca">    
+        <li><a className="botonete" href="#">Novedades</a></li>
+        <li><a className="botonete" href="#">Productos</a></li>
+        <li><a className="botonete" href="#">Servicios</a></li>
+        <li><a className="botonete" href="#">Encontranos</a></li>
         </ul> 
     </nav>
 
